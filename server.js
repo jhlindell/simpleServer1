82b71c8cacdeeaad8d58d2bceca1c53f20be526a
Jon Lindell <jhlindell@yahoo.com>
2017-05-25 09:33:17 -0600

    plug pulled

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static("./index.html"));
+
+app.get("/", function(req, res){
+  res.sendStatus(404);
+});
+
+app.get("/api", function(req, res){
+  res.send("Hello from API");
+});
+
+
 app.listen(8000, function(req, res){
   console.log("listening on port 8000");
 });
